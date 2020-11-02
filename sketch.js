2f8b1eb5fd2ab595814e1813e047c04adfa2d922
Aafiya-Jatu <72740805+Aafiya-Jatu@users.noreply.github.com>
2020-11-02 21:26:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ function setup() {
   
   speed = random(55,90);
   weight = random(400,1500);
+  
+  wall = createSprite(1500,200,60,height/2);
 
   car = createSprite(50,200,50,50);
   car.velocityX = speed;
 
-  wall = createSprite(1500,200,60,height/2);
+  
 }
 
 function draw() {
